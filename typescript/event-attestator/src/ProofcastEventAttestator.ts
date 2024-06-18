abdb421341ab7a6fb8c57e8dbf1b4ba1e0ead398
Mauro Piazza <mauro.piazza.01@gmail.com>
2024-06-18 20:37:08 +0200

    chore(event-attestator): add code

---
+++ typescript/event-attestator/src/ProofcastEventAttestator.ts
@@ -0,0 +1,112 @@
+import crypto from 'crypto'
+import { Event, Signature } from 'ethers'
+import { SigningKey, computeAddress } from 'ethers/lib/utils'
+import { RlpEncode, RlpList } from 'rlp-stream'
+
+import { Chains } from './Chains'
+
+const sha256Digest = (_value: crypto.BinaryLike) => {
+  const sha256 = crypto.createHash('sha256')
+  sha256.update(_value)
+  return sha256.digest()
+}
+
+type Context = {
+  version: number
+  protocolId: number
+  chainId: number
+  privateKey: string | undefined
+}
+
+const fromHex = (_str: string): Buffer => {
+  return Buffer.from(_str.replace('0x', ''), 'hex')
+}
+
+const formatSignature = (_signature: Signature): string => {
+  const r = fromHex(_signature.r)
+  const s = fromHex(_signature.s)
+  const v = Buffer.from([_signature.v])
+  const signature = Buffer.concat([r, s, v], r.length + s.length + v.length)
+  return '0x' + signature.toString('hex')
+}
+
+export class ProofcastEventAttestator {
+  public version: Buffer
+  public protocolId: Buffer
+  public chainId: Buffer
+  public address: string
+  public publicKey: string
+  public privateKey: string
+  private signingKey: SigningKey
+
+  constructor(
+    { version, protocolId, chainId, privateKey }: Context = {
+      version: 0x00,
+      protocolId: 0x00,
+      chainId: Chains.Goerli,
+      privateKey: undefined,
+    },
+  ) {
+    this.version = Buffer.from([version])
+    this.chainId = Buffer.from(chainId.toString(16).padStart(64, '0'), 'hex')
+    this.protocolId = Buffer.from([protocolId])
+    this.privateKey = privateKey
+      ? privateKey
+      : crypto.randomBytes(32).toString('hex')
+    this.signingKey = new SigningKey(Buffer.from(this.privateKey, 'hex'))
+    this.publicKey = this.signingKey.publicKey
+    this.address = computeAddress(this.publicKey)
+  }
+
+  getEventId(event: Event): Buffer {
+    return sha256Digest(event.blockHash)
+  }
+
+  getStatementBytes(event: Event): Buffer {
+    const address = fromHex(event.address)
+    const data = fromHex(event.data)
+    const topics: RlpList = event.topics.map((topic) => fromHex(topic))
+    const eventRLP: RlpList = [address, topics, data]
+    const eventBytes = RlpEncode(eventRLP)
+
+    const eventId = this.getEventId(event)
+    const length =
+      this.version.length +
+      this.protocolId.length +
+      this.chainId.length +
+      eventId.length +
+      eventBytes.length
+
+    return Buffer.concat(
+      [this.version, this.protocolId, this.chainId, eventId, eventBytes],
+      length,
+    )
+  }
+
+  getStatement(event: Event): string {
+    return '0x' + this.getStatementBytes(event).toString('hex')
+  }
+
+  getCommitmentBytes(event: Event): Buffer {
+    const statement = this.getStatementBytes(event)
+    return sha256Digest(statement)
+  }
+
+  getCommitment(event: Event): string {
+    return '0x' + this.getCommitmentBytes(event).toString('hex')
+  }
+
+  signBytes(bytes: Buffer): string {
+    const digest = sha256Digest(bytes)
+    const signature = this.signingKey.signDigest(digest)
+
+    return formatSignature(signature)
+  }
+
+  sign(event: Event): string {
+    const commitment = this.getCommitmentBytes(event)
+    const signature = this.signingKey.signDigest(commitment)
+
+    return formatSignature(signature)
+  }
+}
---
+++ typescript/event-attestator/src/index.ts
@@ -0,0 +1,1 @@
+export { ProofcastEventAttestator } from './ProofcastEventAttestator.js'
