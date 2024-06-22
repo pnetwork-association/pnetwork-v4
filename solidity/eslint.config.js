9f6acab98a5c8b0319910e623ee2a69195df7b6d
Mauro Piazza <mauro.piazza.01@gmail.com>
2024-06-22 11:09:51 +0200

    chore(solidity): ignore linting fox XERC20 related contracts

--- solidity/eslint.config.js
+++ solidity/eslint.config.js
@@ -1,3 +1,9 @@
 import config from '../eslint.config.js'
 
-export default [...config, { rules: { 'no-console': 'off' } }]
+export default [
+  ...config,
+  { rules: { 'no-console': 'off' } },
+  {
+    ignores: ['**/XERC20.sol', '**/XERC20Lockbox.sol'],
+  },
+]
