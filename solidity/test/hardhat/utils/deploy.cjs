d4ea31deb71d77aec74f72dccd439a938f1e0aa7
Mauro Piazza <mauro.piazza.01@gmail.com>
2024-08-06 13:18:06 +0200

    fix(test): forked environment configuration

--- solidity/test/hardhat/utils/deploy.cjs
+++ solidity/test/hardhat/utils/deploy.cjs
@@ -1,4 +1,8 @@
 module.exports.deploy = (_hre, _factory, _args = []) =>
-  _hre.ethers
-    .getContractFactory(_factory)
-    .then(_factory => _factory.deploy(..._args))
+  _hre.ethers.getContractFactory(_factory).then(_factory =>
+    _factory.deploy(..._args, {
+      // Fix an unsufficient maxFeePerGas value
+      // on the fork environment tests
+      maxFeePerGas: process.env.FORK ? 5000000000 : undefined,
+    }),
+  )
