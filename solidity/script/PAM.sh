2a3c156adea8383201fbb74b42916539210c5e2d
Mauro Piazza <mauro.piazza.01@gmail.com>
2024-08-11 17:07:34 +0200

    chore(scripts): add setPAM fn

---
+++ solidity/script/PAM.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+./Script.sh "PAM.s.sol" "$@"
--- solidity/script/XERC20.s.sol
+++ solidity/script/XERC20.s.sol
@@ -2,9 +2,9 @@
 pragma solidity ^0.8.13;
 
 import "forge-std/Script.sol";
-import {IXERC20} from "../src/interfaces/IXERC20.sol";
+import {XERC20} from "../src/xerc20/XERC20.sol";
 
-contract XERC20 is Script {
+contract XERC20Script is Script {
     function setLimits(
         address xerc20,
         address adapter,
@@ -12,13 +12,19 @@ contract XERC20 is Script {
         uint256 burningLimit
     ) external {
         vm.startBroadcast();
-        IXERC20(xerc20).setLimits(adapter, mintingLimit, burningLimit);
+        XERC20(xerc20).setLimits(adapter, mintingLimit, burningLimit);
         vm.stopBroadcast();
     }
 
     function setLockbox(address xerc20, address lockbox) external {
         vm.startBroadcast();
-        IXERC20(xerc20).setLockbox(lockbox);
+        XERC20(xerc20).setLockbox(lockbox);
+        vm.stopBroadcast();
+    }
+
+    function setPAM(address xerc20, address adapter, address pam) external {
+        vm.startBroadcast();
+        XERC20(xerc20).setPAM(adapter, pam);
         vm.stopBroadcast();
     }
 }
