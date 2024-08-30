7f72a1eff1d489fd59d572d693a96ad1cfcf568e
envin <envin@pm.me>
2024-08-30 19:24:18 +0200

    fix(scripts): fix contract paths

--- solidity/scripts/ERC20.s.sol
+++ solidity/scripts/ERC20.s.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.13;
 
 import "forge-std/Script.sol";
-import "../contracts/test/ERC20Test.sol";
+import "../src/contracts/test/ERC20Test.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract ERC20Script is Script {
