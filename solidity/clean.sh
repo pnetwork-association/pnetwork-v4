25cae4cb5bb71804e85bb7a1bef811ca114d296e
Mauro Piazza <mauro.piazza.01@gmail.com>
2024-08-30 16:28:57 +0200

    chore(solidity): improve coverage

---
+++ solidity/clean.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+rm -rf coverage.json \
+    storageLayout \
+    cache_hardhat \
+    artifacts \
+    cache \
+    lcov \
+    out
--- solidity/coverage.sh
+++ solidity/coverage.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
 # Generates local lcov.info
-forge coverage --skip src/ptoken-v1/*.sol src/ptoken-v1/*.sol --report lcov
+forge clean && forge coverage \
+    --skip \
+        src/contracts/ptoken-v1/*.sol \
+        src/contracts/ptoken-v1/*.sol \
+        src/contracts/test/*.sol \
+        test/forge/DeployHelper.sol \
+        test/forge/Helper.sol \
+    --report lcov
 
 # Generate coverage/lcov.info
-FORK=1 yarn hardhat coverage --solcoverjs .solcover.cjs
+npx hardhat clean && FORK=1 npx hardhat coverage --solcoverjs .solcover.cjs
 
 # Generate report
 genhtml -o lcov lcov.info coverage/lcov.info
