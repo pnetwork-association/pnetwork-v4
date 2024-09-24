7aa42297e97a022733e9da02b7927aa03ad3c1a4
Mauro Piazza <mauro.piazza.01@gmail.com>
2024-09-24 10:24:39 +0200

    chore(test): add `getAccountsBalances` util

--- cpp/test/utils/get-token-balance.js
+++ cpp/test/utils/get-token-balance.js
@@ -1,11 +1,32 @@
 const { Asset, Name } = require('@wharfkit/antelope')
 
+const getAccountsBalances = (_accounts, _tokensAndSymbol) => {
+  const res = {}
+  for (let account of _accounts) {
+    res[account] = {}
+    for (let token of _tokensAndSymbol) {
+      res[account][token.symbol] = getTokenBalance(
+        token.contract,
+        account,
+        token.symbol,
+      ).toString()
+    }
+  }
+
+  return res
+}
+
 // Given contract must be compatible with
 // eosio.token
-module.exports.getTokenBalance = (contract, account, symcode) => {
+const getTokenBalance = (contract, account, symcode) => {
   const scope = Name.from(account).value.value
   const primary_key = Asset.SymbolCode.from(symcode).value.value
   const row = contract.tables.accounts(scope).getTableRow(primary_key)
   if (!row) return 0
   return Asset.from(row.balance)
+}
+
+module.exports = {
+  getAccountsBalances,
+  getTokenBalance,
 }
