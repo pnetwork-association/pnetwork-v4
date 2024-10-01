5c49d13bfdf9fecb853d032a4ddc82f7a781998e
envin <envin@pm.me>
2024-10-01 14:35:38 +0200

    test(feesmanager): update tests

--- cpp/test/feesmanager.test.js
+++ cpp/test/feesmanager.test.js
@@ -34,8 +34,6 @@ describe('Feesmanager testing', () => {
     contract: undefined,
   }
 
-  const blockchain = new Blockchain()
-
   const user = 'user'
   const evil = 'evil'
   const issuer = 'issuer'
@@ -45,7 +43,8 @@ describe('Feesmanager testing', () => {
   const node1 = 'nodeone'
   const node2 = 'nodetwo'
 
-  before(async () => {
+  beforeEach(async () => {
+    const blockchain = new Blockchain()
     blockchain.createAccounts(user, evil, issuer, node1, recipient, securitycouncil)
     feesmanager.contract = deploy(blockchain, feesmanager.account, 'contracts/build/feesmanager')
     token.contract = deploy(blockchain, token.account, 'contracts/build/eosio.token')
@@ -57,71 +56,196 @@ describe('Feesmanager testing', () => {
   })
 
   describe('feesmanager::setallowance', () => {
+    it('Should reject if not authorized', async () => {
+      const allowanceValue = "100.0000 TKN"
+      const action = feesmanager.contract.actions
+        .setallowance([node1, token.account, allowanceValue])
+        .send(active(evil))
+
+      await expectToThrow(action, errors.AUTH_MISSING(feesmanager.account))
+    })
+
+    it('Should reject if allowance set is bigger than feesmanager balance', async () => {
+      const allowanceValue = "200.0000 TKN"
+      const feesmanagerBalance = "100.0000 TKN"
+      const assetSymbol = Asset.from(feesmanagerBalance).symbol.code.value.toString()
+
+      await token.contract.actions
+        .create([issuer, token.maxSupply]).send()
+
+      const balanceBefore = token.contract.tables.accounts(getAccountCodeRaw(feesmanager.account)).getTableRow(assetSymbol)
+      expect(balanceBefore).to.equal(undefined)
+
+      await token.contract.actions
+        .issue([issuer, feesmanagerBalance, ''])
+        .send(active(issuer))
+      await token.contract.actions
+        .transfer([issuer, feesmanager.account, feesmanagerBalance, ''])
+        .send(active(issuer))
+
+      const balance = token.contract.tables.accounts(getAccountCodeRaw(feesmanager.account)).getTableRow(assetSymbol)
+      expect(balance).to.be.deep.equal({
+        balance: feesmanagerBalance,
+      })
+
+      const action = feesmanager.contract.actions
+        .setallowance([node1, token.account, allowanceValue])
+        .send(active(feesmanager.account))
+
+      await expectToThrow(action, errors.INSUFFICIENT_BALANCE)
+    })
+
     it('Should set the correct allowance', async () => {
       const allowanceValue = "100.0000 TKN"
+      const feesmanagerBalance = "100.0000 TKN"
 
+      await token.contract.actions
+        .create([issuer, token.maxSupply]).send()
+      await token.contract.actions
+        .issue([issuer, feesmanagerBalance, ''])
+        .send(active(issuer))
+      await token.contract.actions
+        .transfer([issuer, feesmanager.account, feesmanagerBalance, ''])
+        .send(active(issuer))
       await feesmanager.contract.actions
-        .setallowance([node1, allowanceValue])
+        .setallowance([node1, token.account, allowanceValue])
         .send(active(feesmanager.account))
 
       const assetSymbol = Asset.from(allowanceValue).symbol.code.value.toString()
       const allowance = feesmanager.contract.tables.allowances(getAccountCodeRaw(node1)).getTableRow(assetSymbol)
       expect(allowance).to.be.deep.equal({
         allowance_data: allowanceValue,
+        token: token.account,
       })
     })
 
     it('Should update the correct allowance', async () => {
       const allowanceValue = "100.0000 TKN"
+      const feesmanagerBalance = "400.0000 TKN"
+
+      await token.contract.actions
+        .create([issuer, token.maxSupply]).send()
+      await token.contract.actions
+        .issue([issuer, feesmanagerBalance, ''])
+        .send(active(issuer))
+      await token.contract.actions
+        .transfer([issuer, feesmanager.account, feesmanagerBalance, ''])
+        .send(active(issuer))
       await feesmanager.contract.actions
-        .setallowance([node1, allowanceValue])
+        .setallowance([node1, token.account, allowanceValue])
         .send(active(feesmanager.account))
 
       const assetSymbol = Asset.from(allowanceValue).symbol.code.value.toString()
       const allowance = feesmanager.contract.tables.allowances(getAccountCodeRaw(node1)).getTableRow(assetSymbol)
       expect(allowance).to.be.deep.equal({
         allowance_data: allowanceValue,
+        token: token.account,
       })
 
       const updatedAllowanceValue = "300.0000 TKN"
       await feesmanager.contract.actions
-        .setallowance([node1, updatedAllowanceValue])
+        .setallowance([node1, token.account,  updatedAllowanceValue])
         .send(active(feesmanager.account))
 
       const updatedAllowance = feesmanager.contract.tables.allowances(getAccountCodeRaw(node1)).getTableRow(assetSymbol)
       expect(updatedAllowance).to.be.deep.equal({
         allowance_data: updatedAllowanceValue,
+        token: token.account,
       })
     })
   })
 
   describe('feesmanager::incallowance', () => {
+    it('Should reject if not authorized', async () => {
+      const allowanceValue = "100.0000 TKN"
+      const action = feesmanager.contract.actions
+        .incallowance([node1, token.account, allowanceValue])
+        .send(active(evil))
+
+      await expectToThrow(action, errors.AUTH_MISSING(feesmanager.account))
+    })
+
+    it('Should reject if allowance plus inc value is bigger than feesmanager balance', async () => {
+      const allowanceValue = "100.0000 TKN"
+      const feesmanagerBalance = "100.0000 TKN"
+      const assetSymbol = Asset.from(feesmanagerBalance).symbol.code.value.toString()
+
+      await token.contract.actions
+        .create([issuer, token.maxSupply]).send()
+      await token.contract.actions
+        .issue([issuer, feesmanagerBalance, ''])
+        .send(active(issuer))
+      await token.contract.actions
+        .transfer([issuer, feesmanager.account, feesmanagerBalance, ''])
+        .send(active(issuer))
+
+      await feesmanager.contract.actions
+        .setallowance([node1, token.account, allowanceValue])
+        .send(active(feesmanager.account))
+
+      const balance = token.contract.tables.accounts(getAccountCodeRaw(feesmanager.account)).getTableRow(assetSymbol)
+      expect(balance).to.be.deep.equal({
+        balance: feesmanagerBalance,
+      })
+
+      const updatedAllowanceValue = "1.0000 TKN"
+      const action = feesmanager.contract.actions
+        .incallowance([node1, token.account, updatedAllowanceValue])
+        .send(active(feesmanager.account))
+
+      await expectToThrow(action, errors.INSUFFICIENT_BALANCE)
+    })
+
     it('Should increase the allowance', async () => {
       const allowanceValue = "100.0000 TKN"
+      const feesmanagerBalance = "400.0000 TKN"
+
+      await token.contract.actions
+        .create([issuer, token.maxSupply]).send()
+      await token.contract.actions
+        .issue([issuer, feesmanagerBalance, ''])
+        .send(active(issuer))
+      await token.contract.actions
+        .transfer([issuer, feesmanager.account, feesmanagerBalance, ''])
+        .send(active(issuer))
       await feesmanager.contract.actions
-        .setallowance([node1, allowanceValue])
+        .setallowance([node1, token.account, allowanceValue])
+        .send(active(feesmanager.account))
+      await feesmanager.contract.actions
+        .setallowance([node1, token.account, allowanceValue])
         .send(active(feesmanager.account))
 
       const assetSymbol = Asset.from(allowanceValue).symbol.code.value.toString()
       const allowance = feesmanager.contract.tables.allowances(getAccountCodeRaw(node1)).getTableRow(assetSymbol)
       expect(allowance).to.be.deep.equal({
         allowance_data: allowanceValue,
+        token: token.account,
       })
 
       const updatedAllowanceValue = "300.0000 TKN"
       await feesmanager.contract.actions
-        .incallowance([node1, updatedAllowanceValue])
+        .incallowance([node1, token.account, updatedAllowanceValue])
         .send(active(feesmanager.account))
 
       const updatedAllowance = feesmanager.contract.tables.allowances(getAccountCodeRaw(node1)).getTableRow(assetSymbol)
       const expectedAllowanceValue = "400.0000 TKN"
       expect(updatedAllowance).to.be.deep.equal({
         allowance_data: expectedAllowanceValue,
+        token: token.account,
       })
     })
   })
 
   describe('feesmanager::withdrawto', () => {
+    it('Should reject if no allowance found', async () => {
+      const tokenSymbol = Asset.from(`0.000 ${token.symbol}`).symbol
+      const action = feesmanager.contract.actions
+        .withdrawto([node1, token.account, tokenSymbol])
+        .send(active(feesmanager.account))
+
+      await expectToThrow(action, errors.NO_ALLOWANCE_SET)
+    })
+
     it('Should withdraw to the node', async () => {
       const feesmanagerBalance = "100.0000 TKN" 
       await token.contract.actions
@@ -135,33 +259,36 @@ describe('Feesmanager testing', () => {
 
       const allowanceValue = "100.0000 TKN"
       await feesmanager.contract.actions
-        .setallowance([node1, allowanceValue])
+        .setallowance([node1, token.account, allowanceValue])
         .send(active(feesmanager.account))
 
-      const assetSymbol = Asset.from(allowanceValue).symbol.code.value.toString()
-      const allowance = feesmanager.contract.tables.allowances(getAccountCodeRaw(node1)).getTableRow(assetSymbol)
+      const assetSymbol = Asset.from(allowanceValue).symbol
+      const assetSymbolCode = assetSymbol.code.value.toString()
+      const allowance = feesmanager.contract.tables.allowances(getAccountCodeRaw(node1)).getTableRow(assetSymbolCode)
       expect(allowance).to.be.deep.equal({
         allowance_data: allowanceValue,
+        token: token.account,
       })
 
       const feesmanagerBalanceBefore = await token.contract.tables.accounts(getAccountCodeRaw(feesmanager.account)).getTableRows()
       const nodeBalanceBefore = await token.contract.tables.accounts(getAccountCodeRaw(node1)).getTableRows()
-      const allowanceBefore = feesmanager.contract.tables.allowances(getAccountCodeRaw(node1)).getTableRow(assetSymbol)
+      const allowanceBefore = feesmanager.contract.tables.allowances(getAccountCodeRaw(node1)).getTableRow(assetSymbolCode)
       expect(feesmanagerBalanceBefore[0]).to.be.deep.equal({
         balance: feesmanagerBalance,
       })
       expect(nodeBalanceBefore[0]).to.be.deep.equal(undefined)
       expect(allowanceBefore).to.be.deep.equal({
         allowance_data: allowanceValue,
+        token: token.account,
       })
       
       await feesmanager.contract.actions
-        .withdrawto([node1, token.account, allowanceValue])
+        .withdrawto([node1, token.account, assetSymbol])
         .send(active(feesmanager.account))
 
       const feesmanagerBalanceAfter = await token.contract.tables.accounts(getAccountCodeRaw(feesmanager.account)).getTableRows()
       const nodeBalanceAfter = await token.contract.tables.accounts(getAccountCodeRaw(node1)).getTableRows()
-      const allowanceAfter = feesmanager.contract.tables.allowances(getAccountCodeRaw(node1)).getTableRow(assetSymbol)
+      const allowanceAfter = feesmanager.contract.tables.allowances(getAccountCodeRaw(node1)).getTableRow(assetSymbolCode)
       expect(feesmanagerBalanceAfter[0]).to.be.deep.equal({
         balance: "0.0000 TKN",
       })
@@ -170,7 +297,79 @@ describe('Feesmanager testing', () => {
       })
       expect(allowanceAfter).to.be.deep.equal({
         allowance_data: "0.0000 TKN",
+        token: token.account,
       })
     })
+  })
+  
+  describe('feesmanager::withdrawtomultiple', () => {
+    // it('Should withdraw to the nodes', async () => {
+    //   const feesmanagerBalance = "100.0000 TKN" 
+    //   await token.contract.actions
+    //     .create([issuer, token.maxSupply]).send()
+    //   await token.contract.actions
+    //     .issue([issuer, feesmanagerBalance, ''])
+    //     .send(active(issuer))
+    //   await token.contract.actions
+    //     .transfer([issuer, feesmanager.account, feesmanagerBalance, ''])
+    //     .send(active(issuer))
+
+    //   const allowanceValue1 = "50.0000 TKN"
+    //   await feesmanager.contract.actions
+    //     .setallowance([node1, token.account, allowanceValue1])
+    //     .send(active(feesmanager.account))
+
+    //   const allowanceValue2 = "50.0000 TKN"
+    //   await feesmanager.contract.actions
+    //     .setallowance([node2, token.account, allowanceValue2])
+    //     .send(active(feesmanager.account))
+
+    //   const assetSymbol1 = Asset.from(allowanceValue1).symbol
+    //   const assetSymbol1Code = assetSymbol1.code.value.toString()
+    //   const allowance1 = feesmanager.contract.tables.allowances(getAccountCodeRaw(node1)).getTableRow(assetSymbol1Code)
+    //   expect(allowance1).to.be.deep.equal({
+    //     allowance_data: allowanceValue1,
+    //     token: token.account,
+    //   })
+
+    //   const assetSymbol2 = Asset.from(allowanceValue2).symbol
+    //   const assetSymbol2Code = assetSymbol2.code.value.toString()
+    //   const allowance2 = feesmanager.contract.tables.allowances(getAccountCodeRaw(node1)).getTableRow(assetSymbol2Code)
+    //   expect(allowance2).to.be.deep.equal({
+    //     allowance_data: allowanceValue1,
+    //     token: token.account,
+    //   })
+
+    //   const feesmanagerBalanceBefore = await token.contract.tables.accounts(getAccountCodeRaw(feesmanager.account)).getTableRows()
+    //   const nodeBalanceBefore1 = await token.contract.tables.accounts(getAccountCodeRaw(node1)).getTableRows()
+    //   const allowanceBefore1 = feesmanager.contract.tables.allowances(getAccountCodeRaw(node1)).getTableRow(assetSymbol1Code)
+    //   expect(feesmanagerBalanceBefore[0]).to.be.deep.equal({
+    //     balance: feesmanagerBalance,
+    //   })
+    //   expect(nodeBalanceBefore1[0]).to.be.deep.equal(undefined)
+    //   expect(allowanceBefore1).to.be.deep.equal({
+    //     allowance_data: allowanceValue1,
+    //     token: token.account,
+    //   })
+      
+    //   await feesmanager.contract.actions
+    //     .withdrawto([node1, [token.account, token.account], [assetSymbol1, assetSymbol2]])
+    //     .send(active(feesmanager.account))
+
+    //   console.log(feesmanager.contract.bc.console)
+      // const feesmanagerBalanceAfter = await token.contract.tables.accounts(getAccountCodeRaw(feesmanager.account)).getTableRows()
+      // const nodeBalanceAfter = await token.contract.tables.accounts(getAccountCodeRaw(node1)).getTableRows()
+      // const allowanceAfter = feesmanager.contract.tables.allowances(getAccountCodeRaw(node1)).getTableRow(assetSymbol)
+      // expect(feesmanagerBalanceAfter[0]).to.be.deep.equal({
+      //   balance: "0.0000 TKN",
+      // })
+      // expect(nodeBalanceAfter[0]).to.be.deep.equal({
+      //   balance: feesmanagerBalance,
+      // })
+      // expect(allowanceAfter).to.be.deep.equal({
+      //   allowance_data: "0.0000 TKN",
+      //   token: token.account,
+      // })
+    // })
   })
 })
--- cpp/test/utils/errors.js
+++ cpp/test/utils/errors.js
@@ -1,5 +1,7 @@
 const SYMBOL_ALREADY_EXISTS = 'eosio_assert: token with symbol already exists'
 const ACCOUNT_DOES_NOT_EXIST = 'eosio_assert: token account does not exist'
+const INSUFFICIENT_BALANCE = 'eosio_assert: balance is lower than the allowance to be set'
+const NO_ALLOWANCE_SET = 'eosio_assert: No allowance set for this node'
 
 const AUTH_MISSING = _account => `missing required authority ${_account}`
 const SYMBOL_NOT_FOUND = 'eosio_assert: symbol not found'
@@ -9,4 +11,6 @@ module.exports = {
   SYMBOL_NOT_FOUND,
   SYMBOL_ALREADY_EXISTS,
   ACCOUNT_DOES_NOT_EXIST,
+  INSUFFICIENT_BALANCE,
+  NO_ALLOWANCE_SET,
 }
