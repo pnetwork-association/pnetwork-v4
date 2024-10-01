09b527b29db91504fb3e773a6b0274f4c1567967
envin <envin@pm.me>
2024-10-01 14:35:16 +0200

    feat(feesmanager): update table and checks

--- cpp/contracts/feesmanager.cpp
+++ cpp/contracts/feesmanager.cpp
@@ -2,42 +2,49 @@
 
 using namespace eosio;
 
-// Set allowance for a node
-void feesmanager::setallowance( name node, const asset& value ) {
+void feesmanager::setallowance( name node, name token, const asset& value ) {
     require_auth(get_self());
+
+    auto balance = getbalance(token, value.symbol);
+    check(balance.amount >= value.amount, "balance is lower than the allowance to be set");
+
     allowances_table allowances(get_self(), node.value);
     auto allowance_itr = allowances.find(value.symbol.code().raw());
-    
-    if (allowance_itr == allowances.end()) {
+    auto allowance_name_idx = allowances.get_index<"name"_n>();
+    auto allowance_name_itr = allowance_name_idx.find(token.value);
+
+    if (allowance_itr == allowances.end() && allowance_name_itr == allowance_name_idx.end()) {
         allowances.emplace(get_self(), [&](auto& a) {
             a.allowance_data = value;
+            a.token = token;
         });
     } else {
+        check(allowance_itr->token == token, "symbol and token do not match");
         allowances.modify(allowance_itr, get_self(), [&](auto& a) {
             a.allowance_data = value;
         });
     }
 }
 
-// Increase allowance for a node
-void feesmanager::incallowance( name node, const asset& value ) {
+void feesmanager::incallowance( name node, name token, const asset& value ) {
     require_auth(get_self());
 
     allowances_table allowances(get_self(), node.value);
     const auto& allowance_table = allowances.get(value.symbol.code().raw(), "No allowance set for this node");
+
+    auto balance = getbalance(token, value.symbol);
+    check(balance.amount >= value.amount + allowance_table.allowance_data.amount, "balance is lower than the allowance to be set");
 
     allowances.modify(allowance_table, get_self(), [&](auto& a) {
         a.allowance_data.amount += value.amount;
     });
 }
 
-// Withdraw function to withdraw tokens
-void feesmanager::withdrawto( name node, name token, const asset& value ) {
+void feesmanager::withdrawto( name node, name token, symbol token_symbol ) {
     allowances_table allowances(get_self(), node.value);
-    const auto& allowance_table = allowances.get(value.symbol.code().raw(), "No allowance set for this node");
-    check(allowance_table.allowance_data.amount >= 0, "Allowance is zero");
 
-    asset asset_data = asset(allowance_table.allowance_data.amount, value.symbol);
+    const auto& allowance_table = allowances.get(token_symbol.code().raw(), "No allowance set for this node");
+    asset asset_data = asset(allowance_table.allowance_data.amount, token_symbol);
 
     allowances.modify(allowance_table, node, [&](auto& a) {
         a.allowance_data.amount = 0;
@@ -51,8 +58,19 @@ void feesmanager::withdrawto( name node, name token, const asset& value ) {
     ).send();
 }
 
-void feesmanager::withdrawto(name node, const std::vector<name>& tokens, const std::vector<asset>& values) {
-    for (const auto& token : tokens) {
-        withdrawto(node, token, values[0]);
+void feesmanager::withdrawto(name node, const std::vector<name>& tokens, const std::vector<symbol>& token_symbols) {
+    check(tokens.size() == token_symbols.size(), "Token names and symbol size mismatch");
+    for (size_t i = 0; i < tokens.size(); ++i) {
+        withdrawto(node, tokens[i], token_symbols[i]);
+    }
+}
+
+asset feesmanager::getbalance(name token, symbol token_symbol) {
+    accounts accountstable(token, get_self().value);
+    auto it = accountstable.find(token_symbol.code().raw());
+    if (it != accountstable.end()) {
+        return it->balance;
+    } else {
+        return asset(0, token_symbol);
     }
 }
--- cpp/contracts/feesmanager.hpp
+++ cpp/contracts/feesmanager.hpp
@@ -10,21 +10,32 @@ CONTRACT feesmanager : public eosio::contract {
 public:
     using contract::contract;
 
-    ACTION setallowance(name node, const asset& value);
+    ACTION setallowance(name node, name token, const asset& value);
 
-    ACTION incallowance(name node, const asset& value);
+    ACTION incallowance(name node, name token, const asset& value);
 
-    ACTION withdrawto(name node, name token, const asset& value);
+    ACTION withdrawto(name node, name token, symbol token_symbol);
 
-    ACTION withdrawto(name node, const std::vector<name>& tokens, const std::vector<asset>& values);
+    ACTION withdrawto(name node, const std::vector<name>& tokens, const std::vector<symbol>& token_symbols);
+
+    [[eosio::action]]
+    asset getbalance(name token, symbol token_symbol);
 
 private:
+    TABLE account {
+        asset balance;
+
+        uint64_t primary_key()const { return balance.symbol.code().raw(); }
+    };
 
     TABLE allowance {
         asset allowance_data;
+        name token;
 
         uint64_t primary_key()const { return allowance_data.symbol.code().raw(); }
+        uint64_t get_name()const { return token.value; }
     };
 
-    typedef eosio::multi_index< "allowances"_n, allowance > allowances_table;
+    typedef eosio::multi_index<"accounts"_n, account> accounts;
+    typedef eosio::multi_index<"allowances"_n, allowance, eosio::indexed_by<"name"_n, eosio::const_mem_fun<allowance, uint64_t, &allowance::get_name>>> allowances_table;
 };
