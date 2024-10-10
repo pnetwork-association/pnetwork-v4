bb72d72efbcfc5d64167cba0552d565454df9bcc
envin <envin@pm.me>
2024-10-10 15:05:19 +0200

    test: add test data

--- cpp/test/utils/get-operation-sample.js
+++ cpp/test/utils/get-operation-sample.js
@@ -5,19 +5,19 @@ const { no0x, utf8HexString } = require('./wharfkit-ext')
 
 const getOperationSample = _injectedOperation =>
   R.mergeDeepRight(_injectedOperation, {
-    blockId: no0x(zeroPadValue('0x00', 32)),
-    txId: no0x(zeroPadValue('0x00', 32)),
+    blockId: no0x(zeroPadValue('0x21d41bf94358b9252115aee1eb250ef5a644e7fae776b3de508aacda5f4c26fc', 32)),
+    txId: no0x(zeroPadValue('0x6be2de7375ad7c18fd5ca3ecc8b70e60c535750b042200070dc36f84175a16d6', 32)),
     nonce: 0,
     token: no0x(
       zeroPadValue(toBeHex(getSymbolCodeRaw('0.0000 TKN').toString()), 32),
     ),
     originChainId: no0x(
       zeroPadValue(
-        '0xaca376f206b8fc25a6ed44dbdc66547c36c6c33e3a119ffbeaef943642f0e906',
+        '0x01',
         32,
       ),
-    ), // EOS chain id
-    destinationChainId: no0x(zeroPadValue('0x01', 32)), // ETH chain id
+    ), // ETH chain id
+    destinationChainId: no0x(zeroPadValue('0xaca376f206b8fc25a6ed44dbdc66547c36c6c33e3a119ffbeaef943642f0e906', 32)), // EOS chain id
     amount: parseUnits('10', 18).toString(),
     sender: no0x(zeroPadValue(utf8HexString('user'), 32)),
     recipient: no0x(zeroPadValue('0xC0FFEE', 20)),
