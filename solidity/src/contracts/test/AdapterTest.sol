cebe6a423bb86b5880f9371cad0213afa086da7f
envin <envin@pm.me>
2024-10-24 17:35:25 +0200

    test(evm): update tests

--- solidity/src/contracts/test/AdapterTest.sol
+++ solidity/src/contracts/test/AdapterTest.sol
@@ -13,14 +13,28 @@ contract AdapterTest {
         string memory recipient,
         bytes memory data
     ) public {
-        emit IAdapter.Swap(
-            nonce,
-            bytes32(uint256(uint160(erc20))),
+        bytes32 topic0 = bytes32(0x66756E6473206172652073616675207361667520736166752073616675202E2E);
+        bytes memory eventBytes = bytes.concat(
+            bytes32(abi.encode(erc20)),
             bytes32(destination),
             bytes32(amount),
             bytes32(uint256(uint160(sender))),
+            bytes32(bytes(recipient).length),
             bytes(recipient),
+            bytes32(bytes(data).length),
             data
         );
+        assembly {
+            // For memory bytes, skip the length prefix (32 bytes)
+            let dataStart := add(eventBytes, 32)
+            let length := mload(eventBytes)
+
+            log2(
+                dataStart,
+                length,
+                topic0,
+                nonce
+            )
+        }
     }
 }
