e6c7c1d0cf21daae885a834ab4b04db0a59030c0
envin <envin@pm.me>
2024-10-26 01:30:18 +0200

    chore(evm): add comment for custom swap event

--- solidity/src/interfaces/IAdapter.sol
+++ solidity/src/interfaces/IAdapter.sol
@@ -21,6 +21,9 @@ interface IAdapter {
     event PAMChanged(address pamAddress);
     event Settled(bytes32 indexed eventId);
     event FeesManagerChanged(address newAddress);
+    // Low level event
+    // Swap(bytes) 
+    // topic: 0x66756E6473206172652073616675207361667520736166752073616675202E2E
 
     error NotAContract(address addr);
     error NotAllowed();
