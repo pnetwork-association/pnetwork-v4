e9c8e9343d51b31dcc6ec138e9275847732ca048
Mauro Piazza <mauro.piazza.01@gmail.com>
2024-10-30 10:02:40 +0100

    fix(event-attestator): external import

---
+++ javascript/event-attestator/index.js
@@ -0,0 +1,11 @@
+const { Chains } = require('./src/Chains')
+const { Protocols } = require('./src/Protocols')
+const { Versions } = require('./src/Versions')
+const { ProofcastEventAttestator } = require('./src/ProofcastEventAttestator')
+
+module.exports = {
+  Chains,
+  Protocols,
+  Versions,
+  ProofcastEventAttestator,
+}
