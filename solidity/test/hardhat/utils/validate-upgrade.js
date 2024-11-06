88e0109f113be379bf34e551dbe24ac9ba46a0f2
Mauro Piazza <mauro.piazza.01@gmail.com>
2024-11-06 22:37:15 +0100

    chore(solidity): run prettier

--- solidity/test/hardhat/utils/validate-upgrade.js
+++ solidity/test/hardhat/utils/validate-upgrade.js
@@ -1,8 +1,10 @@
 const R = require('ramda')
 
 module.exports.validateUpgrade = R.curry((_hre, _contractName, _proxy) =>
-  _hre.ethers.getContractFactory(_contractName)
-    .then(_contractFactory => _hre.upgrades.validateUpgrade(_proxy, _contractFactory))
-    .catch(_err => Promise.reject(_err))
-
+  _hre.ethers
+    .getContractFactory(_contractName)
+    .then(_contractFactory =>
+      _hre.upgrades.validateUpgrade(_proxy, _contractFactory),
+    )
+    .catch(_err => Promise.reject(_err)),
 )
