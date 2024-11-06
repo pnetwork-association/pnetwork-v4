80c61fd8264392a7ff6736f853d9157355a5a597
Mauro Piazza <mauro.piazza.01@gmail.com>
2024-11-06 21:55:49 +0100

    chore(get-operation-sample): rm overhead

--- cpp/test/utils/get-operation-sample.js
+++ cpp/test/utils/get-operation-sample.js
@@ -8,16 +8,14 @@ const getOperationSample = _injectedOperation =>
     blockId: '21d41bf94358b9252115aee1eb250ef5a644e7fae776b3de508aacda5f4c26fc',
     txId: '6be2de7375ad7c18fd5ca3ecc8b70e60c535750b042200070dc36f84175a16d6',
     nonce: 0,
-    token: no0x(
-      zeroPadValue(toBeHex('0xe58cBE144dD5556C84874deC1b3F2d0D6Ac45F1b'), 32),
-    ),
-    originChainId: no0x(zeroPadValue('0x01', 32)), // ETH chain id
+    token: '0x000000000000000000000000e58cbe144dd5556c84874dec1b3f2d0d6ac45f1b',
+    originChainId:
+      '0000000000000000000000000000000000000000000000000000000000000001', // ETH chain id
     destinationChainId:
       'aca376f206b8fc25a6ed44dbdc66547c36c6c33e3a119ffbeaef943642f0e906', // EOS chain id
     amount: '5.889675 XTKN',
-    sender: no0x(
-      zeroPadValue('0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266', 32),
-    ),
+    sender:
+      '0x000000000000000000000000f39fd6e51aad88f6f4ce6ab8827279cfffb92266',
     recipient: 'destinatieos',
     data: '',
   })
