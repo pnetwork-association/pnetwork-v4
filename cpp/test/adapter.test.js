a69393a334359c18a21560b2676e689ea3292e87
Mauro Piazza <mauro.piazza.01@gmail.com>
2024-11-08 11:57:08 +0100

    chore(adapter.test.js): rm samples dependency

--- cpp/test/adapter.test.js
+++ cpp/test/adapter.test.js
@@ -3,16 +3,21 @@ const { Blockchain, expectToThrow } = require('@eosnetwork/vert')
 const { Asset } = require('@wharfkit/antelope')
 const { Symbol } = Asset
 const {
+  sum,
   no0x,
   active,
   deploy,
   errors,
+  bytes32,
+  getOperation,
   getSymbolCodeRaw,
   getAccountCodeRaw,
+  serializeOperation,
+  getAccountsBalances,
   fromEthersPublicKey,
   getSingletonInstance,
 } = require('./utils')
-const { toBeHex, zeroPadValue, stripZerosLeft } = require('ethers')
+const { toBeHex, stripZerosLeft } = require('ethers')
 const {
   Chains,
   Versions,
@@ -42,6 +47,11 @@ describe('Adapter tests', () => {
   const recipient = 'eosrecipient'
   const feemanager = 'feemanager'
 
+  const evmAdapter =
+    '000000000000000000000000bcf063a9eb18bc3c6eb005791c61801b7cb16fe4'
+  const evmTopicZero =
+    '66756e6473206172652073616675207361667520736166752073616675202e2e'
+
   const notLocalToken = {
     symbol,
     account: '',
@@ -54,9 +64,7 @@ describe('Adapter tests', () => {
     symbol,
     account: tokenAccount,
     maxSupply: Asset.from(maxSupply, symbolPrecision),
-    bytes: no0x(
-      zeroPadValue(toBeHex(String(getSymbolCodeRaw(symbolPrecision))), 32),
-    ),
+    bytes: no0x(bytes32(toBeHex(String(getSymbolCodeRaw(symbolPrecision))))),
   }
 
   const xerc20 = {
@@ -99,7 +107,7 @@ describe('Adapter tests', () => {
     chainId: Chains(Protocols.Eos).Mainnet,
   })
 
-  ;[NOT_LOCAL, LOCAL].map(_type => {
+  ;[NOT_LOCAL].map(_type => {
     describe(_type, () => {
       before(() => {
         blockchain.createAccounts(
@@ -425,13 +433,7 @@ describe('Adapter tests', () => {
       })
 
       describe('adapter::setorigin', () => {
-        const originChainId = no0x(
-          zeroPadValue(Chains(Protocols.Evm).Mainnet, 32),
-        )
-        const evmAdapter =
-          '000000000000000000000000bcf063a9eb18bc3c6eb005791c61801b7cb16fe4'
-        const evmTopicZero =
-          '66756e6473206172652073616675207361667520736166752073616675202e2e'
+        const originChainId = no0x(bytes32(Chains(Protocols.Evm).Mainnet))
 
         it('Should throw if called by not authorized account', async () => {
           const action = adapter.contract.actions
@@ -470,7 +472,7 @@ describe('Adapter tests', () => {
 
         it('Should set the origin details correctly', async () => {
           const anAddress = no0x(
-            zeroPadValue('0xe396757ec7e6ac7c8e5abe7285dde47b98f22db8', 32),
+            bytes32('0xe396757ec7e6ac7c8e5abe7285dde47b98f22db8'),
           )
           await adapter.contract.actions
             .setorigin([originChainId, anAddress, evmTopicZero])
@@ -497,6 +499,117 @@ describe('Adapter tests', () => {
           expect(emitterRow.chain_id).to.be.equal(originChainId)
           expect(emitterRow.emitter).to.be.equal(evmAdapter)
           expect(emitterRow.topic_zero).to.be.equal(evmTopicZero)
+        })
+      })
+
+      describe('adapter::settle', () => {
+        const operation = getOperation({
+          blockId:
+            '7e21ba208ea2a072bad2d011dbc3a9f870c574a66203d84bde926fcf85756d78',
+          txId: '2e3704b180feda25af9dfe50793e292fd99d644aa505c3d170fa69407091dbd3',
+          nonce: 0,
+          token: '0x810090f35dfa6b18b5eb59d298e2a2443a2811e2',
+          originChainId: Chains(Protocols.Evm).Mainnet, // EVM mainnet chain id
+          destinationChainId: Chains(Protocols.Eos).Mainnet,
+          amount: Asset.from(5.87190615, xsymbolPrecision),
+          sender:
+            '000000000000000000000000f39fd6e51aad88f6f4ce6ab8827279cfffb92266',
+          recipient,
+          data: '',
+        })
+
+        const event = {
+          blockHash: operation.blockId,
+          transactionHash: operation.txId,
+          address: evmAdapter,
+          topics: [evmTopicZero],
+          data: serializeOperation(operation),
+        }
+        const preimage = no0x(evmEA.getEventPreImage(event))
+        const signature = no0x(evmEA.formatEosSignature(evmEA.sign(event)))
+
+        const metadata = { preimage, signature }
+
+        it('Should reject if adapter and token do not match', async () => {
+          const wrongTokenBytes = no0x(bytes32('0x'))
+          const action = adapter.contract.actions
+            .settle([
+              user,
+              { ...no0x(operation), token: wrongTokenBytes },
+              metadata,
+            ])
+            .send(active(user))
+
+          await expectToThrow(action, errors.INVALID_TOKEN)
+        })
+
+        it('Should settle the operation properly', async () => {
+          const before = getAccountsBalances(
+            [user, recipient, adapter.account, feemanager],
+            [xerc20],
+          )
+
+          await adapter.contract.actions
+            .settle([user, no0x(operation), metadata])
+            .send(active(user))
+
+          const after = getAccountsBalances(
+            [user, recipient, adapter.account, feemanager],
+            [xerc20],
+          )
+
+          expect(after[recipient][xerc20.symbol]).to.be.equal(
+            operation.amount.toString(),
+          )
+
+          expect(after[adapter.account][xerc20.symbol]).to.be.equal(
+            `0.0000 ${xerc20.symbol}`,
+          )
+
+          expect(before[feemanager][xerc20.symbol]).to.be.equal(
+            after[feemanager][xerc20.symbol],
+          )
+        })
+
+        it('Should settle the operation properly and send userdata', async () => {
+          const operationWithUserData = {
+            ...operation,
+            data: '0x12345abcdefc0de1337f',
+          }
+
+          const newEvent = {
+            ...event,
+            data: serializeOperation(operationWithUserData),
+          }
+
+          const newMetadata = {
+            preimage: evmEA.getEventPreImage(newEvent),
+            signature: evmEA.formatEosSignature(evmEA.sign(newEvent)),
+          }
+
+          const before = getAccountsBalances(
+            [user, recipient, adapter.account],
+            [xerc20],
+          )
+
+          const beforeAsset = Asset.from(before[recipient][xerc20.symbol])
+
+          await adapter.contract.actions
+            .settle([user, no0x(operationWithUserData), no0x(newMetadata)])
+            .send(active(user))
+
+          const after = getAccountsBalances(
+            [user, recipient, adapter.account],
+            [xerc20],
+          )
+
+          expect(after[recipient][xerc20.symbol]).to.equal(
+            sum(operation.amount, beforeAsset).toString(),
+          )
+
+          expect(after[adapter.account][xerc20.symbol]).to.be.equal(
+            `0.0000 ${xerc20.symbol}`,
+          )
         })
       })
     })
