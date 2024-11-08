b307f826aa45fb81da4f2cbedd269fcbd4e1acb8
Mauro Piazza <mauro.piazza.01@gmail.com>
2024-11-08 11:55:29 +0100

    chore(bytes-utils.js): improve `no0x` & `_0x`

--- cpp/test/utils/bytes-utils.js
+++ cpp/test/utils/bytes-utils.js
@@ -1,3 +1,6 @@
+const R = require('ramda')
+const { zeroPadValue } = require('ethers')
+
 const getXbytesHex = (hex, offset, byteNum) =>
   hex.slice(offset * 2, offset * 2 + byteNum * 2)
 
@@ -9,14 +12,47 @@ const hexToString = hex => {
   return str
 }
 
-const no0x = _0xValue => _0xValue.replace('0x', '')
+const replaceOrIdentity = (_regExp, _value, _x) => {
+  try {
+    return R.replace(_regExp, _value, _x)
+  } catch (e) {
+    return _x
+  }
+}
+
+// If input is an object, then
+// { k: '0x1111' }
+//
+// becomes
+//
+// { k: '1111'}
+//
+const no0x = _0xValue =>
+  R.type(_0xValue) === 'Object'
+    ? R.keys(_0xValue).reduce(
+        (acc, elem) => ({
+          ...acc,
+          [elem]: replaceOrIdentity('0x', '', _0xValue[elem]),
+        }),
+        _0xValue,
+      )
+    : _0xValue.replace('0x', '')
+
+const _0x = _value =>
+  _value instanceof Buffer
+    ? `0x${no0x(_value.toString('hex'))}`
+    : `0x${no0x(_value)}`
 
 const hexStringToBytes = _hex => Uint8Array.from(Buffer.from(no0x(_hex), 'hex'))
 
 const removeNullChars = string => string.replace(/\u0000/g, '')
 
+const bytes32 = _value => zeroPadValue(_0x(_value), 32)
+
 module.exports = {
+  _0x,
   no0x,
+  bytes32,
   getXbytesHex,
   hexToString,
   hexStringToBytes,
