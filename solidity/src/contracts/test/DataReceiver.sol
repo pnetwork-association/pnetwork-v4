b032864d6d5bfdb2260b34b1053e29a05571686c
gitmp01 <mauro.piazza.01@gmail.com>
2025-03-04 22:23:39 +0100

    feat(IPReceiver): add `UserData` struct
    This will add more useful fields to the IAdapter.Operation.data
    in order to provide the contract receiving data more details
    to validate

--- solidity/src/contracts/test/DataReceiver.sol
+++ solidity/src/contracts/test/DataReceiver.sol
@@ -4,8 +4,9 @@ pragma solidity ^0.8.0;
 import {IPReceiver} from "../../interfaces/IPReceiver.sol";
 
 contract DataReceiver is IPReceiver {
-    event DataReceived(bytes userdata);
-    function receiveUserData(bytes calldata userdata) external {
-        emit DataReceived(userdata);
+    event DataReceived(IPReceiver.UserData);
+
+    function receiveUserData(IPReceiver.UserData memory userData) external {
+        emit DataReceived(userData);
     }
 }
